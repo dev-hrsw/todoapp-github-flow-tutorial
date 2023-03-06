727431adf51bdd4a4e8b087e0f42841e72889735
hrswmsys <aq.m1xup@gmail.com>
2023-03-06 18:07:11 +0900

    colorfunction  changed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ $("#addTodo").click(() => {
 $(document).on("change", "input[type=checkbox]", () => {
     if($(this).is(":checked")) {
         $(this).parent().css("text-decoration", "line-through");
+        $(this).parent().css("color", "#CCC");
     } else {
         $(this).parent().css("text-decoration", "none");
     }
